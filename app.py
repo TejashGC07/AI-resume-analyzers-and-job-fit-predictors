3eac3827c07a66789d503e312f82eb9616224761
TejashGC07 <120017241+TejashGC07@users.noreply.github.com>
2026-05-04 15:17:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from main import extract_skills, similarity_score, missing_skills
+from main import extract_skills, similarity_score, missing_skills, suggestions, weighted_score
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
-    resume = request.form["resume"]
-    job = request.form["job"]
+    resume = request.form["resume"].lower()
+    job = request.form["job"].lower()
 
-    skills = extract_skills(resume.lower())
-    score = similarity_score(resume.lower(), job.lower())
-    missing = missing_skills(skills, job.lower())
+    skills = extract_skills(resume)
+    score = similarity_score(resume, job)
+    weight = weighted_score(skills)
+    missing = missing_skills(skills, job)
+    tips = suggestions(score, missing)
+
+    final_score = round((score * 0.6 + weight * 0.4), 2)
 
     return jsonify({
-        "score": round(score,2),
+        "score": final_score,
         "skills": skills,
-        "missing": missing
+        "missing": missing,
+        "tips": tips
     })
 
 if __name__ == "__main__":
