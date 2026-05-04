cc7978205b151c75e7a370c16edf01c0c439b227
TejashGC07 <120017241+TejashGC07@users.noreply.github.com>
2026-05-04 12:54:13 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from main import extract_skills, similarity_score, missing_skills
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    resume = request.form["resume"]
+    job = request.form["job"]
+
+    skills = extract_skills(resume.lower())
+    score = similarity_score(resume.lower(), job.lower())
+    missing = missing_skills(skills, job.lower())
+
+    return jsonify({
+        "score": round(score,2),
+        "skills": skills,
+        "missing": missing
+    })
+
+app.run()
